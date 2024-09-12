5fe884c4c607ad8e11c3a1de9eb187f3d5a5ef78
Nathan Sousa <nathan.wks@outlook.com>
2024-09-12 11:06:18 -0300

    add exercise hello world

---
+++ src/iniciante/Iniciante001HelloWorld.java
@@ -0,0 +1,8 @@
+package iniciante;
+import java.io.IOException;
+
+public class Iniciante001HelloWorld {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Hello World!");
+    }
+}
